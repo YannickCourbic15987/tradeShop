7d7c5f2438d18788aa0fe41f469295e6beba302b
= <=>
2022-10-10 16:30:00 +0200

    geoApi construction

--- assets/js/geoApi.js
+++ assets/js/geoApi.js
@@ -2,3 +2,7 @@ const codePostal = document.getElementById("add_addresses_PostalCode");
 codePostal.addEventListener("input", (e) => {
   console.log(e.target.value);
 });
+const apiUrl = `https://geo.api.gouv.fr/communes?codePostal`;
+//créer une requête
+let request = new XMLHttpRequest();
+request.open("", ""); // paramétre GET / POST
